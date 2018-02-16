3d7f07286d1489be35ed3293ace822808df1ac47
André Schreck <andre.schreck@gmail.com>
2018-02-16 21:22:04 +0100

    initial commit

---
+++ src/main/java/breakfast/hackers/superballservice/SuperballServiceApplication.java
@@ -0,0 +1,12 @@
+package breakfast.hackers.superballservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperballServiceApplication.class, args);
+	}
+}
