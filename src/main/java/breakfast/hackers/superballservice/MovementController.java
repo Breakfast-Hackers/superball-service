8930e3ac9452ecfa53bb0fcfa84dc45fbb1ab15b
André Schreck <andre.schreck@gmail.com>
2018-02-16 23:18:13 +0100

    alexa endpoint

--- src/main/java/breakfast/hackers/superballservice/MovementController.java
+++ src/main/java/breakfast/hackers/superballservice/MovementController.java
@@ -1,6 +1,11 @@
 package breakfast.hackers.superballservice;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping(path="/api/movements")
 public class MovementController {
     
+    private Queue<MovementDTO> movementQueue = new ArrayDeque<>();
+    
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
     public void move(MovementDTO movement) {
+        movementQueue.offer(movement);
+    }
+    
+    @MessageMapping("/api/movementSocket")
+    @SendTo("/topic/movements")
+    public MovementDTO connect() {
+        MovementDTO movement = movementQueue.poll();
+        while(movement == null) {
+            sleep(250L);
+            movement = movementQueue.poll();
+        }
+        return movement;
+    }
+
+    private void sleep(long i) {
+        // TODO Auto-generated method stub
         
     }
 }
--- src/main/java/breakfast/hackers/superballservice/SuperballServiceApplication.java
+++ src/main/java/breakfast/hackers/superballservice/SuperballServiceApplication.java
@@ -1,12 +1,29 @@
 package breakfast.hackers.superballservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 
 @SpringBootApplication
 public class SuperballServiceApplication {
+    
+    @Autowired
+    private SuperballSpeechlet superballSpeechlet;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperballServiceApplication.class, args);
 	}
+	
+	@Bean
+    public ServletRegistrationBean registerServlet() {
+        SpeechletServlet speechletServlet = new SpeechletServlet();
+        speechletServlet.setSpeechlet(superballSpeechlet);
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(speechletServlet, "/api/movements");
+        return servletRegistrationBean;     
+    }
 }
