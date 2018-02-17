d63996ac42377ff81d74e429dc59c98e64cf3d3c
André Schreck <andre.schreck@gmail.com>
2018-02-17 18:44:45 +0100

    get top ten

--- src/main/java/breakfast/hackers/superballservice/highscore/HighscoreController.java
+++ src/main/java/breakfast/hackers/superballservice/highscore/HighscoreController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HighscoreController implements HighscoreObserver {
@@ -20,6 +22,12 @@ public class HighscoreController implements HighscoreObserver {
     @PostConstruct
     public void init() {
         highscoreService.addObserver(this);
+    }
+    
+    @GetMapping(path="/api/highscores", produces="application/json")
+    public ResponseEntity<HighscoreDTO> getTopTen() {
+        List<Long> scores = highscoreService.getTopTen();
+        return ResponseEntity.ok(new HighscoreDTO(scores));
     }
     
     @Override
