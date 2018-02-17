4e82c2ba8ffab751962625b8494ac1ac51afafd9
André Schreck <andre.schreck@gmail.com>
2018-02-17 19:12:29 +0100

    initialize top ten

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	var socket = new SockJS('/superball-websocket');
+const initTopTen = () => {
+    fetch('https://superball.herokuapp.com//api/highscores', {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    })
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(json => {
+        $('#highscoreContainer').html('');
+        json.scores.forEach(score => $('#highscoreContainer').append('<div>' + score + '</div>'));
+    });
+}
+
+const initWebSockets = () => {
+    var socket = new SockJS('/superball-websocket');
 	stompClient = Stomp.over(socket);
 	stompClient.connect({}, function (frame) {
 	    console.log('Connected: ' + frame);
@@ -7,26 +24,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    	const movement = JSON.parse(message.body)
 	        $('#movementsContainer').append('<div><span>' + movement.action + '</span></div>');
 	    });
-	    
+
 	    stompClient.subscribe('/topic/commands', function (message) {
 	    	const command = JSON.parse(message.body)
 	        $('#commandsContainer').append('<div><span>' + command.action + '</span></div>');
 	    });
-	    
+
 	    stompClient.subscribe('/topic/obstacles', function (message) {
 	    	const obstacle = JSON.parse(message.body)
 	        $('#obstaclesContainer').append('<div style="background-color:' + obstacle.color + '"><span>' + obstacle.position + '</span></div>');
 	    });
-	    
+
 	    stompClient.subscribe('/topic/duration', function (message) {
 	    	const duration = JSON.parse(message.body)
 	        $('#durationContainer').html('<div>' + duration.duration + '</div>');
 	    });
-	    
+
 	    stompClient.subscribe('/topic/highscore', function (message) {
 	    	const highscore = JSON.parse(message.body)
 	    	$('#highscoreContainer').html('');
 	    	highscore.scores.forEach(score => $('#highscoreContainer').append('<div>' + score + '</div>'));
 	    });
 	});
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    initTopTen();
+    initWebSockets();
 });
